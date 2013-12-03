8ddb71fe00a5a393070085f9f848ee019f7798ab
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 15:37:02 +0100

    Finished ProjectEuler3Fast but it is not so fast :'(

--- project-euler-3/src/main/java/pl/grzeslowski/project_euler_3/Main.java
+++ project-euler-3/src/main/java/pl/grzeslowski/project_euler_3/Main.java
@@ -3,7 +3,7 @@ package pl.grzeslowski.project_euler_3;
 public class Main {
 
     public static void main(final String[] args) {
-        final long l = 6_008_514_143L;
+        final long l = 600_851_475_143L;
 
         try {
             System.out.println(new ProjectEuler3SimpleImpl()
--- project-euler-3/src/main/java/pl/grzeslowski/project_euler_3/ProjectEuler3Faster.java
+++ project-euler-3/src/main/java/pl/grzeslowski/project_euler_3/ProjectEuler3Faster.java
@@ -7,10 +7,34 @@ public class ProjectEuler3Faster implements ProjectEuler3 {
         if (value <= 0) {
             throw new IllegalArgumentException();
         }
-        
-        while (true) {
-            return 0;
+
+        final Prime prime = new Prime();
+
+        // if we get prime w know that its biggest factor is it
+        if (prime.isPrime(value)) {
+            return value;
         }
+
+        long biggestResult = -1;
+
+        long dividedValue = value;
+        long primeDivider = 2;
+        while (!prime.isPrime(dividedValue)) {
+            if (dividedValue % primeDivider == 0) {
+                dividedValue /= primeDivider;
+
+                if (primeDivider > biggestResult) {
+                    biggestResult = primeDivider;
+                }
+
+                primeDivider = 2;
+            } else {
+                primeDivider = prime.getNextPrime(primeDivider);
+            }
+
+        }
+
+        return biggestResult > dividedValue ? biggestResult : dividedValue;
     }
 
 }
