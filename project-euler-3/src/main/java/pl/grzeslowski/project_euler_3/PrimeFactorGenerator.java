18d216cf3c8c40586eae9b0f851b39ae556e60d7
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 13:34:01 +0100

    Createt tests

--- project-euler-3/src/main/java/pl/grzeslowski/project_euler_3/PrimeFactorGenerator.java
+++ project-euler-3/src/main/java/pl/grzeslowski/project_euler_3/PrimeFactorGenerator.java
@@ -2,7 +2,7 @@ package pl.grzeslowski.project_euler_3;
 
 public final class PrimeFactorGenerator {
 
-    public long findFirstSmallerFactor(final long value) {
+    public long findFirstSmallerPrime(final long value) {
         return 0L;
     }
 }
---
+++ project-euler-3/src/test/java/pl/grzeslowski/project_euler_3/PrimeFactorGeneratorTest.java
@@ -0,0 +1,70 @@
+package pl.grzeslowski.project_euler_3;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.fest.assertions.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(value = Parameterized.class)
+public final class PrimeFactorGeneratorTest {
+    private final int in;
+    private final long out;
+
+    public PrimeFactorGeneratorTest(final int in, final long out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        final Object[][] data = new Object[][] { { 2, 2 }, { 14, 13 } };
+        return Arrays.asList(data);
+    }
+
+    @Test
+    public void test_normal_parameters() {
+
+        // given
+        final PrimeFactorGenerator generator = new PrimeFactorGenerator();
+
+        // when
+        final long prime = generator.findFirstSmallerPrime(in);
+
+        // then
+        Assertions.assertThat(prime).isEqualTo(out);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_maxValue_equals_0() {
+
+        // given
+        final PrimeFactorGenerator generator = new PrimeFactorGenerator();
+
+        // when
+        generator.findFirstSmallerPrime(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_maxValue_equals_minus_1() {
+
+        // given
+        final PrimeFactorGenerator generator = new PrimeFactorGenerator();
+
+        // when
+        generator.findFirstSmallerPrime(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_maxValue_equals_1() {
+
+        // given
+        final PrimeFactorGenerator generator = new PrimeFactorGenerator();
+
+        // when
+        generator.findFirstSmallerPrime(1);
+    }
+}
