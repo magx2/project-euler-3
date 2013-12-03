742dddf77825f65655d29ad83b0eb622b614a5ce
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 22:06:10 +0100

    Adding test for new way of solving problem

---
+++ project-euler-3/src/test/java/pl/grzeslowski/project_euler_3/ProjectEuler3DiffrentWayTest.java
@@ -0,0 +1,62 @@
+package pl.grzeslowski.project_euler_3;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.fest.assertions.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(value = Parameterized.class)
+public class ProjectEuler3DiffrentWayTest {
+    private final int in;
+    private final long out;
+
+    public ProjectEuler3DiffrentWayTest(final int in, final long out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        final Object[][] data = new Object[][] { { 2, 2 }, { 3, 3 }, { 4, 2 },
+                { 5, 5 }, { 6, 3 }, { 9, 3 }, { 14, 7 } , {21, 7}};
+        return Arrays.asList(data);
+    }
+
+    @Test
+    public void test_normal_parameters() {
+
+        // given
+        final ProjectEuler3 euler = new ProjectEuler3DiffrentWay();
+
+        // when
+        final long primeFactor = euler.findLargestPrimeFactor(in);
+
+        // then
+        Assertions.assertThat(primeFactor).isEqualTo(out);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_maxValue_equals_0() {
+
+        // given
+        final ProjectEuler3 euler = new ProjectEuler3DiffrentWay();
+
+        // when
+        euler.findLargestPrimeFactor(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_maxValue_equals_minus_1() {
+
+        // given
+        final ProjectEuler3 euler = new ProjectEuler3DiffrentWay();
+
+        // when
+        euler.findLargestPrimeFactor(-1);
+    }
+
+}
