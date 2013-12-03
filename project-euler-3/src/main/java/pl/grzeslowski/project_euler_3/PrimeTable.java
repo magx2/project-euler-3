75e2fb4050b099debf10d547dad208551a398766
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 15:37:18 +0100

    Adding new class PrimeTable

---
+++ project-euler-3/src/main/java/pl/grzeslowski/project_euler_3/PrimeTable.java
@@ -0,0 +1,9 @@
+package pl.grzeslowski.project_euler_3;
+
+public final class PrimeTable {
+
+    public long getBiggestPrime(final long value) {
+        return 0;
+    }
+    
+}
---
+++ project-euler-3/src/test/java/pl/grzeslowski/project_euler_3/PrimeTableTest.java
@@ -0,0 +1,5 @@
+package pl.grzeslowski.project_euler_3;
+
+public class PrimeTableTest {
+
+}
